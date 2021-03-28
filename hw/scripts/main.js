aa8c6925b31986df1bc8cf4057d977b6f80a908f
Chelsea <870313amy@gmail.com>
2021-03-28 23:01:14 +0800

    hw2

---
+++ hw/scripts/main.js
@@ -0,0 +1,79 @@
+//date
+var today = new Date();
+var date = today.getFullYear()+'/'+(today.getMonth()+1)+'/'+today.getDate();
+var time = today.getHours() + ":" + today.getMinutes() ;
+document.getElementById("time").innerHTML = date+'  '+time;
+
+//counter
+var n = localStorage.getItem('on_load_counter');
+
+if (n == null) {
+  n = 0;
+}
+n++;
+
+localStorage.setItem("on_load_counter", n);
+
+nums = n.toString().split('').map(Number);
+document.getElementById('CounterVisitor').innerHTML = '';
+for (var i of nums) {
+  var temp = document.getElementById('CounterVisitor').innerHTML+'<span class="counter-item">' + i + '</span>';
+  document.getElementById('CounterVisitor').innerHTML = temp;
+}
+
+//photo slider
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+
+// Personalized welcome message code
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+  let myName = prompt('Please enter your name.');
+  if(!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem('name', myName);
+    myHeading.innerHTML = 'Matcha is tasty, ' + myName;
+  }
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem('name');
+  myHeading.innerHTML = 'Matcha is tasty, ' + storedName;
+}
+
+
+
+
+myButton.onclick = function() {
+  setUserName();
+}
